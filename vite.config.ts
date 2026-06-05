c2aa49906f7825729f8da6e864977d0b80462127
taisan11 <131413638+taisan11@users.noreply.github.com>
2026-06-05 20:10:41 +0900

    cf setup 忘れ

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import {cloudflare} from "@cloudflare/vite-plugin"
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), cloudflare()],
 })
